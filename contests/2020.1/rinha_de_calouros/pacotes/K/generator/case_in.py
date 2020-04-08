5e33d04c981d7297698d837b6df5d38b3069af27
Lais <lais@vvossen.com>
2020-04-08 17:08:37 -0300

    mudanças nas restrições e casos

--- contests/2020.1/rinha_de_calouros/pacotes/K/generator/case_in.py
+++ contests/2020.1/rinha_de_calouros/pacotes/K/generator/case_in.py
@@ -2,9 +2,9 @@ from random import randint as rand
 
 cases = int(input("Gerar quantos casos? -> "))
 
-for number in range(4, 1+cases):
+for number in range(5, 5+cases):
     file = open('../input/' + str(number), 'w')
-    S = rand(1,80)
+    S = rand(1,200)
     # S = 200
     file.write(str(S) + '\n')
     for i in range(S):
